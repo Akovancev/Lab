cb596e1a984fdc5de4f1813ba5e0c2584d32d8ca
Акованцев Павел Максимович <45533870+Akovancev@users.noreply.github.com>
2020-02-29 10:03:27 +0300

    0

---
+++ 2 КУРС/Второй семестр/ЯМП/lab1/lab1/lab1.cpp
@@ -0,0 +1,139 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+int* Read(ifstream& f, int n)
+{
+    int* a = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        f >> a[i];
+    }
+    return a;
+}
+void Write(ofstream& f, int* a, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        f << a[i] << ' ';
+    }
+    f << endl;
+}
+int Multiple(int* a, int n)
+{
+    int m = 1;
+    for (int i = 0; i < n; i++)
+    {
+        m *= a[i];
+    }
+    return m;
+}
+int Create_file_and_Razm() {
+    ifstream f("input.txt");
+    ofstream f1("res.txt");
+    int n = 0;
+    int a;
+    while (!f.eof())
+    {
+        f >> a;
+        n++;
+        f1 << a << ' ';
+    }
+    n = sqrt(n);
+    return n;
+}
+
+
+
+void Partition(int count, int n) {
+    ifstream f("res.txt");
+    ofstream a("a.txt");
+    ofstream b("b.txt");
+    int* mas = Read(f, n);
+    while (!f.eof()) {
+        for (int i = 0; i < count && (!f.eof()); i++) {
+            Write(a, mas, n);
+            mas = Read(f, n);
+        }
+        for (int j = 0; j < count && !f.eof(); j++) {
+            Write(b, mas, n);
+            mas = Read(f, n);
+        }
+    }
+    a.close();
+    b.close();
+    f.close();
+}
+
+void Merge(int count, int n)
+{
+    ifstream a("a.txt");
+    ifstream b("b.txt");
+    ofstream f("res.txt");
+    int* mas1 = new int[n];
+    int* mas2 = new int[n];
+    if (!a.eof())
+        mas1 = Read(a, n);
+    if (!b.eof())
+        mas2 = Read(b, n);
+    while (!a.eof() && !b.eof())
+    {
+        int i = 0;
+        int j = 0;
+        while (i < count && j < count && !a.eof() && !b.eof())
+        {
+            if (Multiple(mas1, n) < Multiple(mas2, n))
+            {
+                Write(f, mas1, n);
+                mas1 = Read(a, n);
+                i++;
+            }
+            else
+            {
+                Write(f, mas2, n);
+                mas2 = Read(b, n);
+                j++;
+            }
+        }
+        while (i < count && !a.eof())
+        {
+            Write(f, mas1, n);
+            mas1 = Read(a, n);
+            i++;
+        }
+        while (j < count && !b.eof()) {
+            Write(f, mas2, n);
+            mas2 = Read(b, n);
+            j++;
+        }
+    }
+    while (!a.eof())
+    {
+        Write(f, mas1, n);
+        mas1 = Read(a, n);
+    }
+    while (!b.eof())
+    {
+        Write(f, mas2, n);
+        mas2 = Read(b, n);
+    }
+    a.close();
+    b.close();
+    f.close();
+}
+int main()
+{
+    setlocale(0, "");
+    int n = Create_file_and_Razm();
+    cout << "Размерность матрицы:" << n << endl;
+    for (int k = 1; k < n; k *= 2)
+    {
+        Partition(k, n);
+        Merge(k, n);
+    }
+    cout << "Готово!";
+    return 0;
+}
---
+++ 2 КУРС/Второй семестр/ЯМП/lab2/lab2/lab2.cpp
@@ -0,0 +1,208 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+struct Student
+{
+    string FIO;
+    float mark;
+    Student()
+    {
+        FIO = "";
+    }
+};
+
+void Write(Student* temp, ofstream &f)
+{
+    if (temp != NULL)
+    {
+        f << temp->FIO << temp->mark << endl;
+    }
+}
+
+Student* Read(ifstream& f)
+{
+    string s = "";
+    Student* temp = new Student;
+    getline(f, s);
+    int i = 0;
+    while (i < s.length() && (s[i] < '0' || s[i]>'9'))
+    {
+        temp->FIO += s[i];
+        i++;
+    }
+    if (i == s.length() || (s[i + 1] != '.' && (i + 1) != s.length()))
+    {
+        return NULL;
+    }
+    temp->mark = s[i] - '0';
+    int ten = 10;
+    i+=2;
+    for (; i < s.length(); i++)
+    {
+        float digit = s[i] - '0';
+        temp->mark += digit/ten;
+        ten *= 10;
+    }
+    return temp;
+}
+
+void Create_new_fail()
+{
+    ifstream in("in.txt");
+    ofstream out("res.txt");
+    while (!in.eof())
+    {
+        Write(Read(in), out);
+    }
+}
+
+bool Partition()
+{
+    bool check = true;
+    ifstream f("res.txt");
+    ofstream a("a.txt");
+    ofstream b("b.txt");
+    Student* x = Read(f);
+    Write(x, a);
+    while (!f.eof())
+    {
+        Student* y = x;
+        x = Read(f);
+        while (x != NULL && !f.eof() && y->mark <= x->mark)
+        {
+            Write(x, a);
+            y = x;
+            x = Read(f);
+        }
+        if (x != NULL && y->mark > x->mark)
+        {
+            Write(x, b);
+            check = false;
+        }
+        else Write(x, a);
+        if (f.eof()) break;
+        y = x;
+        x = Read(f);
+        while (x != NULL && !f.eof() && y->mark <= x->mark)
+        {
+            Write(x, b);
+            check = false;
+            y = x;
+            x = Read(f);
+        }
+        if (x != NULL && y->mark > x->mark) Write(x, a);
+        else
+        {
+            Write(x, b);
+            check = false;
+        }
+    }
+    f.close();
+    a.close();
+    b.close();
+    return check;
+}
+
+void Merge()
+{
+    ofstream f("res.txt");
+    ifstream a("a.txt");
+    ifstream b("b.txt");
+    Student* x1 = Read(a);
+    Student* y1 = Read(b);
+    Student* x2;
+    Student* y2;
+    while (!a.eof() && !b.eof())
+    {
+        x2 = x1;
+        y2 = y1;
+        while ((x1 != NULL && y1 != NULL) && x1->mark >= x2->mark && y1->mark >= y2->mark && !a.eof() && !b.eof())
+        {
+            if (x1->mark > y1->mark)
+            {
+                Write(y1, f);
+                y2 = y1;
+                y1 = Read(b);
+            }
+            else
+            {
+                Write(x1, f);
+                x2 = x1;
+                x1 = Read(a);
+            }
+        }
+        while (x1 != NULL && x1->mark >= x2->mark && !a.eof())
+        {
+            Write(x1, f);
+            x2 = x1;
+            x1 = Read(a);
+        }
+        while (y1 != NULL && y1->mark >= y2->mark && !b.eof())
+        {
+            Write(y1, f);
+            y2 = y1;
+            y1 = Read(b);
+        }
+    }
+    if (!a.eof())
+    {
+        while ((x1 != NULL && y1 != NULL) && !a.eof() && x1->mark <= y1->mark)
+        {
+            Write(x1, f);
+            x1 = Read(a);
+        }
+        Write(y1, f);
+        Write(x1, f);
+    }
+    if (!b.eof())
+    {
+        while ((x1 != NULL && y1 != NULL) && !b.eof() && x1->mark >= y1->mark)
+        {
+            Write(y1, f);
+            y1 = Read(b);
+        }
+        Write(x1, f);
+        Write(y1, f);
+    }
+    if (a.eof() && b.eof())
+    {
+        if ((x1 != NULL && y1 != NULL) && x1->mark >= y1->mark)
+        {
+            Write(y1, f);
+            Write(x1, f);
+        }
+        else
+        {
+            Write(x1, f);
+            Write(y1, f);
+        }
+    }
+    while (!a.eof())
+    {
+        x1 = Read(a);
+        Write(x1, f);
+    }
+    while (!b.eof())
+    {
+        y1 = Read(b);
+        Write(y1, f);
+    }
+    f.close();
+    a.close();
+    b.close();
+}
+
+int main()
+{
+    setlocale(0, "");
+    Create_new_fail();
+    while (!Partition())
+    {
+        Merge();
+    }
+    cout << "Готово!!!!!\n\n\n";
+    return 0;
+}
