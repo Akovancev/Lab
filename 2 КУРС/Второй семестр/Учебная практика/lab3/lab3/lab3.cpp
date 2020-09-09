5d5fd4050c4490a23680d1c2b6576fa32a88bcfa
Акованцев Павел Максимович <45533870+Akovancev@users.noreply.github.com>
2020-09-09 19:10:45 +0300

    0

---
+++ 2 КУРС/Второй семестр/Учебная практика/lab3/lab3/lab3.cpp
@@ -0,0 +1,97 @@
+﻿#include <iostream>
+
+using namespace std;
+const int n = 3;
+const int m = 5;
+
+int Sum(int max)
+{
+    int sum = 0;
+    while (max != 0)
+    {
+        sum += max % 10;
+        max /= 10;
+    }
+    return sum;
+}
+
+void Sort(int X[n][m], int** pos)
+{
+    int dop[n];
+    for (int i = 0; i < n; i++)
+    {
+        int max = X[i][0];
+        for (int j = 1; j < m; j++)
+        {
+            if (X[i][j] > max)
+            {
+                max = X[i][j];
+            }
+        }
+        dop[i] = Sum(max);
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (dop[j] < dop[j + 1])
+            {
+                int temp = dop[j];
+                dop[j] = dop[j + 1];
+                dop[j + 1] = temp;
+                int* templine = pos[j];
+                pos[j] = pos[j + 1];
+                pos[j + 1] = templine;
+            }
+        }
+    }
+}
+
+int main()
+{
+    setlocale(0, "");
+    int X[n][m];
+    for (int i = 0; i < n; i++) // инициализация статического массива a
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << "Введите элемент (" << i + 1 << ", " << j + 1 << ")" << endl;
+            cin >> X[i][j];
+        }
+    }
+    int** pos = new int* [n];
+    if (!pos)
+    {
+        cout << "Память не выделенна" << endl;
+        return -1;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        pos[i] = X[i];
+    }
+    cout << "Исходный массив:" << endl; 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << X[i][j] << " ";
+        }
+        cout << endl;
+    }
+    Sort(X, pos);
+    cout << "Итоговый массив:" << endl; 
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << *((pos[i]) + j) << " ";
+        }
+        cout << endl;
+    } 
+    for (int i = 0; i < n; i++) 
+    {
+        delete[] pos[i];
+    } 
+    delete[] pos;
+    return 0;
+}
---
+++ 2 КУРС/Второй семестр/ЯМП/lab8/lab8/lab8.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Структура списка для хранения хеша
+struct Hash
+{
+    int key;
+    int value;
+    Hash* next;
+
+    Hash(int k, int v, Hash* n) // конструктор с параметрами
+    {
+        key = k;
+        value = v;
+        next = n;
+    }
+};
+int m; // размер хеш-таблицы
+int n; // количество элементов таблицы
+Hash** table; // хеш-таблица
+
+// Функция инициализации таблицы
+void null_table()
+{
+    for (int i = 0; i < m; i++)
+    {
+        table[i] = NULL;
+    }
+}
+
+// Функция добавления элемента в таблицу
+void Insert(int key, int value)
+{
+    int hash = (key % m); // считаем хеш методом деления с остатком
+    if (table[hash] == NULL) // если нет элементов с полученным хешем
+    {
+        table[hash] = new Hash(key, value, NULL); // то элемент будет первым в списке
+    }
+    else // если же уже есть элементы с полученным хешем
+    {
+        // то добавляем элемент в начало списка 
+        Hash* list = table[hash];
+        table[hash] = new Hash(key, value, list);
+    }
+    n++; // увеличиваем счетчик элементов на 1
+}
+
+// Функция считывания элементов из файла
+void Read()
+{
+    ifstream f("in.txt");
+    f >> m;
+    table = new Hash * [m];
+    null_table();
+    n = 0; // зануляем количество элементов
+    while (!f.eof())
+    {
+        int i, j;
+        f >> i >> j; // считываем ключ элемента и значение
+        Insert(i, j); // добавляем элемент в таблицу
+    }
+}
+
+// Функция подсчета количества элементов с определенным хешем 
+int Count(int key)
+{
+    int count = 0; // счетчик элементов
+    Hash* list = table[key]; // копируем список, хранящий все элементы с данным хешем
+    while (list != NULL) // считаем сколько элементов с данным хешем
+    {
+        count++;
+        list = list->next;
+    }
+    return count;
+}
+
+// Функция для подсчета необходимого критерия
+double Task()
+{
+    double x = 0;
+    double c = (double)n / m;
+    for (int i = 0; i < m; i++)
+    {
+        int f = Count(i);
+        x += (f - c) * (f - c);
+    }
+    x /= c;
+    return x;
+}
+
+int main()
+{
+    setlocale(0, "");
+    Read();
+    cout << "X2 = " << Task();
+    return 0;
+}
